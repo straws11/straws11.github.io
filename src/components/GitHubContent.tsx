92d3bc727184362e23eb9b3d28fa55e73297590f
straws11 <dswarts11@gmail.com>
2025-07-15 14:38:17 +0200

    Make use of public api for github cards

--- src/components/GitHubContent.tsx
+++ src/components/GitHubContent.tsx
@@ -1,6 +1,5 @@
 import { faGithub } from "@fortawesome/free-brands-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import axios from "axios";
 import { ReactNode, useEffect, useState } from "react";
 
 interface GitHubCardProps {
@@ -18,12 +17,11 @@ function GitHubCard(props: GitHubCardProps) {
             <div className="grid text-xs w-[250px] h-[192px] grid-cols-[200px,35px] sm:text-lg sm:w-full sm:grid-cols-[340px,60px] lg:grid-cols-[380px,83.5px] lg:w-[480px] xl: place-content-between bg-[#8ca9bc] p-2 rounded-3xl shadow-lg transition-transform duration-300 ease-in-out hover:scale-105">
                 <div className="h-[168px] flex flex-col justify-between">
                     <div><h3 className="font-bold tracking-wider m-1 bg-[#3F708F] rounded-2xl p-2">{name}</h3>
-                    <p className="italic">{description}</p></div>
+                        <p className="italic">{description}</p></div>
                     <div>
                         <p>{`Main Language: ${language}`}</p>
-                        <p className="text-right">{`Updated on: ${
-                            pushedDate.split("T")[0]
-                        }`}</p>
+                        <p className="text-right">{`Updated on: ${pushedDate.split("T")[0]
+                            }`}</p>
                     </div>
                 </div>
                 <div className="flex items-center justify-center">
@@ -42,63 +40,12 @@ interface RepoEntry {
     description: string | null;
     url: string;
     language: string | null;
-    pushedAt: string;
+    updatedAt: string;
 }
 
 export default function GitHubContent() {
     const [githubCards, setGithubCards] = useState<ReactNode[]>([]);
 
-    const fetchDataFromLambda = async (): Promise<RepoEntry[]> => {
-        try {
-            const lambdaEndpoint =
-                "https://nbpu7nrcc7rbqvrbpqk3o5ylhq0uojwd.lambda-url.eu-north-1.on.aws/";
-            //make a get request to githubRepos lambda function
-            const response = await axios.get(lambdaEndpoint);
-
-            // handle api response
-            if (response.status === 200) {
-                if (response.data) {
-                    console.log("Lambda Response:", response.data);
-                    return response.data;
-                } else {
-                    throw new Error("Lambda response body is undefined!");
-                }
-            } else {
-                //not 200 code
-                throw new Error(
-                    `Lambda request failed with status ${response.status}`
-                );
-            }
-        } catch (error) {
-            console.error("Error fetching data:", error);
-            return [];
-        }
-    };
-
-    async function getData() {
-        // Get data from either localStorage or make request if outdated/not existing
-        const lastUpdate = localStorage.getItem("lastGitHubRetrieve");
-        const lastUpdateTime = lastUpdate ? parseInt(lastUpdate, 10) : null;
-        const currentTime = Date.now();
-
-        var repoData: RepoEntry[];
-
-        // if outdated, call api again
-        if (
-            !lastUpdateTime ||
-            currentTime - lastUpdateTime > 24 * 60 * 60 * 1000
-        ) {
-            repoData = await fetchDataFromLambda();
-            //update cached data
-            localStorage.setItem("lastGitHubRetrieve", currentTime.toString());
-            localStorage.setItem("repoData", JSON.stringify(repoData));
-        } else {
-            // else retrieve from local storage
-            const cachedData = localStorage.getItem("repoData");
-            repoData = cachedData ? JSON.parse(cachedData) : null;
-        }
-        return repoData;
-    }
 
     useEffect(() => {
         getData().then((repoData: RepoEntry[]) => {
@@ -106,7 +53,7 @@ export default function GitHubContent() {
             const sortedRepoData = repoData
                 .slice()
                 .sort(
-                    (a, b) => Date.parse(b.pushedAt) - Date.parse(a.pushedAt)
+                    (a, b) => Date.parse(b.updatedAt) - Date.parse(a.updatedAt)
                 );
 
             // map to cards
@@ -117,7 +64,7 @@ export default function GitHubContent() {
                     language={repo.language || ""}
                     name={repo.name.toUpperCase()}
                     url={repo.url}
-                    pushedDate={repo.pushedAt}
+                    pushedDate={repo.updatedAt}
                 />
             ));
             setGithubCards(cards);
@@ -140,4 +87,92 @@ export default function GitHubContent() {
             </div>
         </section>
     );
+}
+
+/** Extract fields from github returned json into a repo entry array */
+function extractRepoEntries(data: any): RepoEntry[] {
+    if (data.constructor !== Array) return [];
+    const entries = data.map((repo) => {
+        const entry: RepoEntry = {
+            url: repo.html_url,
+            name: repo.name,
+            language: repo.language,
+            description: repo.description,
+            updatedAt: repo.updated_at,
+        };
+        return entry;
+    });
+    return entries;
+}
+
+/** Make github request and extract relevant data */
+async function fetchDataFromGithub(): Promise<RepoEntry[]> {
+    try {
+        const repoUrl = "https://api.github.com/users/straws11/repos";
+        // make a get request
+        const response = await fetch(repoUrl,
+            {
+                method: "GET",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+            }
+        );
+        // handle api response
+        if (response.ok) {
+            const data = await response.json();
+            if (data === null) {
+                console.error("Response body is undefined!");
+                return [];
+            }
+            // data is present
+            console.log("GitHub Response:", data);
+            const allEntries: RepoEntry[] = extractRepoEntries(data);
+            return removeRedundantRepos(allEntries);
+
+        } else {
+            //not 200 code
+            console.error(`Request failed with status ${response.status}`);
+            return [];
+        }
+    } catch (error) {
+        console.error("Error fetching data:", error);
+        return [];
+    }
+}
+
+/** Get data from either localStorage or make request if outdated/not existing */
+async function getData() {
+    const lastUpdate = localStorage.getItem("lastGitHubRetrieve");
+    const lastUpdateTime = lastUpdate ? parseInt(lastUpdate, 10) : null;
+    const currentTime = Date.now();
+
+    var repoData: RepoEntry[];
+
+    // if outdated, call api again
+    if (
+        !lastUpdateTime ||
+        currentTime - lastUpdateTime > 24 * 60 * 60 * 1000
+    ) {
+        console.log("Calling");
+        repoData = await fetchDataFromGithub();
+        //update cached data
+        localStorage.setItem("lastGitHubRetrieve", currentTime.toString());
+        localStorage.setItem("repoData", JSON.stringify(repoData));
+    } else {
+        // else retrieve from local storage
+        console.log("retrieving");
+        const cachedData = localStorage.getItem("repoData");
+        repoData = cachedData ? JSON.parse(cachedData) : null;
+    }
+    return repoData;
+}
+
+/** Remove repos matching the blacklist, not relevant to show on page */
+function removeRedundantRepos(allEntries: RepoEntry[]): RepoEntry[] {
+    const blacklist: string[] = [
+        "straws11", // homepage
+        "dotfiles", // config
+    ];
+    return allEntries.filter((repo) => !blacklist.includes(repo.name));
 }
--- src/components/Links.tsx
+++ src/components/Links.tsx
@@ -5,28 +5,26 @@ import {
     faYoutube,
 } from "@fortawesome/free-brands-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import ScrollTransition from "./ScrollTransition";
 
 export default function Links() {
-    const size = window.innerWidth > 1024 ? "5x" : "2x";
     return (
         <>
             {/* i can't remember what "title" is for */}
-            <p className="text-black text-center text-md md:text-3xl tracking-widest animate-bounce">
+            <p className="text-black text-md md:text-4xl text-center tracking-widest animate-bounce">
                 Find me here!
             </p>
             <div className="flex gap-11 justify-center p-4">
                 <a href="https://www.github.com/straws11">
-                    <FontAwesomeIcon icon={faGithub} size={size} />
+                    <FontAwesomeIcon icon={faGithub} className="text-3xl lg:text-6xl" />
                 </a>
                 <a href="https://www.linkedin.com/in/dylan-swarts-1011xa">
-                    <FontAwesomeIcon icon={faLinkedinIn} size={size} />
+                    <FontAwesomeIcon icon={faLinkedinIn} className="text-3xl lg:text-6xl" />
                 </a>
                 <a href="https://www.instagram.com/strawsdylan.png/">
-                    <FontAwesomeIcon icon={faInstagram} size={size} />
+                    <FontAwesomeIcon icon={faInstagram} className="text-3xl lg:text-6xl" />
                 </a>
                 <a href="https://www.youtube.com/@DylanSwarts">
-                    <FontAwesomeIcon icon={faYoutube} size={size} />
+                    <FontAwesomeIcon icon={faYoutube} className="text-3xl lg:text-6xl" />
                 </a>
             </div>
         </>
